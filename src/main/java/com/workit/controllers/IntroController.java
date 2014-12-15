f341f76507b66d1d7b604d76cd1644b9f4df0a80
David <drodriguezs87@gmail.com>
2014-12-15 22:10:02 +0100

    Intro

---
+++ src/main/java/com/workit/controllers/IntroController.java
@@ -0,0 +1,27 @@
+package com.workit.controllers;
+
+import java.io.IOException;
+	
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IntroController {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value="/intro.htm")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+         return new ModelAndView("intro");
+    }
+}
